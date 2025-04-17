8e878cb15da7c0403f5256c0ef4a1f7983938636
Orest Yakymiv <akimivorest@gmail.com>
2025-04-17 12:25:30 +0300

    fixed error of autotest

--- task.sql
+++ task.sql
@@ -50,5 +50,5 @@ CREATE TABLE Users (
 --rollback DROP TABLE Users;
 
 --changeset mate.acamemy:6 labels:0.0.3
-CREATE INDEX EmailIndex ON Users (Email);
---rollback DROP INDEX EmailIndex ON Users;
+CREATE INDEX Email ON Users (Email);
+--rollback DROP INDEX Email ON Users;
